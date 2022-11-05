ac9c7a324954456f8f97302798952d27594f0e49
liam smith <liamj.smith@hotmail.com>
2022-11-05 15:37:47 -0600

    checkpoint2 almost done

--- app.js
+++ app.js
@@ -83,25 +83,35 @@ function getBacon() {
 function baconCounter() {
 
   template = `
-<div class="col-12 col-md-2">
+<div class="col-12 bg-dark text-light">
   bacon = ${bacon}
 </div>
 
-<div onclick="buyClickUpgrade('broadsword')" class=" col-2 btn btn-success">
+<div class="d-flex">
+<div class" col-12 col-md-2 ">
+<img class="img-fluid piggy" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlnBtUmXjx9A2bd7N0Poh89aWNBqvy_JgQYoqnhxHcChiwwjLCmLRsrKBOutnyk7IHGGM&usqp=CAU" alt="">
+</div>
+<div class = "col-12 col-md-2 card bg-warning "> <h4 class="p-2">Swanson's bacon Swap</h4>
+<p class="p-2">We trade stuff for bacon</p>
+</div>
+</div>
+
+<div class"col-12 col-md-8"></div>
+<div onclick="buyClickUpgrade('broadsword')" class="btn-around ms-3 col-2 btn btn-success">
   Broadsword $${clickUpgrade[0].price}
 </div>
 
-<div onclick="buyClickUpgrade('lightsaber')" class=" col-2 btn btn-success">
+<div onclick="buyClickUpgrade('lightsaber')" class="btn-around col-2 btn btn-success">
   Lightsaber $${clickUpgrade[1].price}
 </div>
 <div class="col-6></div>
 
 <div  class="col-2> </div>
-<div onclick="buyAutoUpgrade('luffy')" class=" col-2 btn btn-primary">
+<div onclick="buyAutoUpgrade('luffy')" class="btn-around ms-3 col-2 btn btn-primary">
   Luffy $${autoUpgrade[0].price}
 </div>
 
-<div onclick="buyAutoUpgrade('tanjiro')" class=" col-2 btn btn-primary">
+<div onclick="buyAutoUpgrade('tanjiro')" class="btn-around col-2 btn btn-primary">
   Tanjiro $${autoUpgrade[1].price}
 </div>
   `
